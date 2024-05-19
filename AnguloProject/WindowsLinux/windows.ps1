7978dd2d0c295807a852b973f3eab369ca7f0ead
Eduardo Miguel dos Reis Teixeira <eduksoficial@gmail.com>
2024-05-18 22:10:52 -0300

    Update windows.ps1

--- AnguloProject/WindowsLinux/windows.ps1
+++ AnguloProject/WindowsLinux/windows.ps1
@@ -1,7 +1,7 @@
 $folderPath = 'C:\Users\Eduks\Documents\desenvolvimentos\Angulo\AnguloProject' # Substitua pelo caminho da sua pasta
 $wordToUncomment = '//system("cls")'
 
-Get-ChildItem $folderPath -Include *.c, *h -Recurse | ForEach-Object {
+Get-ChildItem $folderPath -Include *.c, *.h -Recurse | ForEach-Object {
     $file = $_
     $text = Get-Content $file
     $text = $text -replace [regex]::Escape($wordToUncomment), 'system("cls")'
